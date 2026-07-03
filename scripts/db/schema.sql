d639d359b2e71f6f9d54a786c71a6ece13ffcd12
Melet Chirino <melet.chirino@gmail.com>
2026-07-03 00:15:19 +0000

    Update sql schema

--- scripts/db/schema.sql
+++ scripts/db/schema.sql
@@ -1,5 +1,9 @@
 CREATE TABLE IF NOT EXISTS conversations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chat_id INTEGER NOT NULL,
     conversation TEXT NOT NULL,
     status BOOLEAN NOT NULL DEFAULT 0
 );
+
+CREATE INDEX IF NOT EXISTS idx_conversations_chat_active
+    ON conversations (chat_id, status);
